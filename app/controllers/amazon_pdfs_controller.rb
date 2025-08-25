330fa532f69657b83a89124c157ce093478df763
KheffyC <kheffy.cervantez@gmail.com>
2025-08-25 21:58:51 +0000

    fixes for Capote

--- app/controllers/amazon_pdfs_controller.rb
+++ app/controllers/amazon_pdfs_controller.rb
@@ -4,6 +4,9 @@ class AmazonPdfsController < ApplicationController
 
   def index
     @amazon_pdfs = AmazonPdf.programless.order(type_of_pdf_group: :desc)
+    group_order = ['Schedules', 'Syllabus']
+    @grouped_amazon_pdfs = @amazon_pdfs.order(:event_date).group_by { |pdf| pdf.type_of_pdf_group }
+    @ordered_pdfs = @grouped_amazon_pdfs.sort_by { |group_name, pdfs| group_order.index(group_name) || 999 }
   end
 
   def student_forms
